853cacbbb45be0c82884aa864ad436ad8aae3aac
gdmec07150623 <2268084817@qq.com>
2016-12-21 11:59:50 +0800

    Initial commit

---
+++ app/src/main/java/com/s07150623/mycontacts/ContactsTable.java
@@ -0,0 +1,37 @@
+package com.s07150623.mycontacts;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+/**
+ * Created by LH on 2016/10/24.
+ */
+public class ContactsTable {
+    private final static String TABLENAME = "contactsTable";
+    private MyDB db;
+
+    public ContactsTable(Context context) {
+        db = new MyDB(context);
+        if (!db.isTableExits(TABLENAME)){
+            String createTableSql = "CREATE TABLE IF NOT EXISTS " +TABLENAME+" (id_DB integer "+
+                    "primary key AUTOINCREMENT,"+
+                    User.NAME+" VARCHAR,"+
+                    User.MOBLIE+" VARCHAR,"+
+                    User.QQ+" VARCHAR,"+
+                    User.DANWEI+" VARCHAR,"+
+                    User.ADDRESS+" VARCHAR)";
+            db.createTable(createTableSql);
+        }
+
+    }
+    public boolean addData(User user){
+        ContentValues values = new ContentValues();
+        values.put(User.NAME,user.getName());
+        values.put(User.MOBLIE,user.getMoblie());
+        values.put(User.DANWEI,user.getDanwei());
+        values.put(User.QQ,user.getQq());
+        values.put(User.ADDRESS,user.getAddress());
+        return db.save(TABLENAME,values);
+    }
+}
+
